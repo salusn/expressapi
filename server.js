fee2f61e82080730dc449fe9141b1ac01941dabb
salusn <salu.s@lightrains.com>
2017-11-24 11:58:26 +0530

    Created model for post method

--- server.js
+++ server.js
@@ -1,40 +1,70 @@
 // jshint ignore: start
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');
+var app        = express();
 var bodyParser = require('body-parser');
-
 var mongoose   = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/expressapi';
-var db = mongoose.connection;
-// mongoose.connect(mongoDB, {
-//   useMongoClient: true
-// });
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var Bear     = require('./models/bear');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+mongoose.Promise = Promise;
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/Bear", {
+  useMongoClient: true,
+  promiseLibrary: global.Promise
+});
+
+var db = mongoose.connection;
+// mongodb error
+db.on('error', console.error.bind(console, 'connection error:'));
+// mongodb connection open
+db.once('open', () => {
+  console.log(`Connected to Mongo at: ${new Date()}`)
+});
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	console.log("connected")
+    next();
+});
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// more routes for our API will happen here
+// router.route('/bears')
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+//     // create a bear (accessed at POST http://localhost:8080/api/bears)
+//     .post(function(req, res) {
+    
+//         var bear = new Bear({
+//         	name : req.body.name,        
+//             value : req.body.value
+//         	}); 
+        
+//         bear.save(function(err) {
+//             if (err)
+//                 res.send(err);
+
+//             res.json({ message: 'Bear created!' });
+//         });
+
+//     });
+
+
 app.use('/api', router);
 
 // START THE SERVER
-// =============================================================================
+
 app.listen(port);
 console.log('Magic happens on port ' + port);
 
